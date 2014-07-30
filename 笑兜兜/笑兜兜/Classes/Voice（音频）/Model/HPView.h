e49cecd011f6b9d55d9a77775b55d6d638a7e4a3
HMFifth <caoxiaobo@cxb.local>
2014-07-30 15:07:56 +0800

    音频模型
    更新

---
+++ 笑兜兜/笑兜兜/Classes/Voice（音频）/Model/HPView.h
@@ -0,0 +1,13 @@
+//
+//  HPView.h
+//  笑兜兜
+//
+//  Created by 曹晓波 on 14-7-30.
+//  Copyright (c) 2014年 cn.happy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPView : UIView
+
+@end
---
+++ 笑兜兜/笑兜兜/Classes/Voice（音频）/Model/HPView.m
@@ -0,0 +1,31 @@
+//
+//  HPView.m
+//  笑兜兜
+//
+//  Created by 曹晓波 on 14-7-30.
+//  Copyright (c) 2014年 cn.happy. All rights reserved.
+//
+
+#import "HPView.h"
+
+@implementation HPView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
